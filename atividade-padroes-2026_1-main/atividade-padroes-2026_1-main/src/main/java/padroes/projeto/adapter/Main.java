5b0c76c1d80d450f2094e62f29179ce1dd49cc7f
Pedro Nunes Valeriano Duarte <125411554+PedrokaIsACoder@users.noreply.github.com>
2026-06-18 17:45:16 -0300

    Update Main.java
    Atualizando a main so pra testar o funcionamento

--- atividade-padroes-2026_1-main/atividade-padroes-2026_1-main/src/main/java/padroes/projeto/adapter/Main.java
+++ atividade-padroes-2026_1-main/atividade-padroes-2026_1-main/src/main/java/padroes/projeto/adapter/Main.java
@@ -2,6 +2,20 @@ package padroes.projeto.adapter;
 
 public class Main {
     public static void main(String[] args){
-  
+        CentralAtendimento central = new CentralAtendimento();
+
+        AtendenteIngles ingles = new AtendenteIngles();
+        AtendentePT adaptadorIngles = new AdaptadorIngles(ingles);
+
+        AtendenteEspanhol espanhol = new AtendenteEspanhol();
+        AtendentePT adaptadorEspanhol = new AdaptadorEspanhol(espanhol);
+
+        System.out.println("=== Atendimento em Inglês ===");
+        central.realizarAtendimento(adaptadorIngles, "payment");
+
+        System.out.println();
+
+        System.out.println("=== Atendimento em Espanhol ===");
+        central.realizarAtendimento(adaptadorEspanhol, "pago");
     }
 }
