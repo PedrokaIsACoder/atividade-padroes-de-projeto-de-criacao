882f7349e2cb9d4245cf84404a796973833b7cb0
Pedro Nunes Valeriano Duarte <125411554+PedrokaIsACoder@users.noreply.github.com>
2026-06-18 17:36:34 -0300

    Update AdaptadorIngles.java
    (Atividade de Padrões de projeto estruturais, adicionando interface para AdaptadorIngles.

--- atividade-padroes-2026_1-main/atividade-padroes-2026_1-main/src/main/java/padroes/projeto/adapter/AdaptadorIngles.java
+++ atividade-padroes-2026_1-main/atividade-padroes-2026_1-main/src/main/java/padroes/projeto/adapter/AdaptadorIngles.java
@@ -1,5 +1,24 @@
 package padroes.projeto.adapter;
 
-public class AdaptadorIngles {
- 
+public class AdaptadorIngles implements AtendentePT {
+   private AtendenteIngles atendente;
+
+    public AdaptadorIngles(AtendenteIngles atendente) {
+        this.atendente = atendente;
+    }
+
+    @Override
+    public void cumprimentar() {
+        atendente.cumprimentar();
+    }
+
+    @Override
+    public void responderDuvida(String duvida) {
+        atendente.responderDuvida(duvida);
+    }
+
+    @Override
+    public void encerrarAtendimento() {
+        atendente.encerrarAtendimento();
+    }
 }
