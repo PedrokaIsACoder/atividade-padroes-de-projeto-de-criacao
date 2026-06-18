d0846c72e27f104e76afbb034026585baf0d2353
Pedro Nunes Valeriano Duarte <125411554+PedrokaIsACoder@users.noreply.github.com>
2026-06-18 17:42:10 -0300

    Update AdaptadorEspanhol.java
    Adicionando interface no arquivo AdaptadorEspanhol.

--- atividade-padroes-2026_1-main/atividade-padroes-2026_1-main/src/main/java/padroes/projeto/adapter/AdaptadorEspanhol.java
+++ atividade-padroes-2026_1-main/atividade-padroes-2026_1-main/src/main/java/padroes/projeto/adapter/AdaptadorEspanhol.java
@@ -1,5 +1,24 @@
 package padroes.projeto.adapter;
 
-public class AdaptadorEspanhol {
+public class AdaptadorEspanhol implements AtendentePT {
+  private AtendenteEspanhol atendente;
 
+    public AdaptadorEspanhol(AtendenteEspanhol atendente) {
+        this.atendente = atendente;
+    }
+
+    @Override
+    public void cumprimentar() {
+        atendente.cumprimentar();
+    }
+
+    @Override
+    public void responderDuvida(String duvida) {
+        atendente.responderDuvida(duvida);
+    }
+
+    @Override
+    public void encerrarAtendimento() {
+        atendente.encerrarAtendimento();
+    }
 }
